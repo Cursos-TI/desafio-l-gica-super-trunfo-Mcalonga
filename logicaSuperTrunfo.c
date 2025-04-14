d285246629c8ecf88c1a10124ba9847d7db8cbef
Mcalonga <71896093+Mcalonga@users.noreply.github.com>
2025-04-14 23:48:36 +0000

    Exercicio Mestre

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -40,24 +40,25 @@ int main() {
     // printf("A cidade vencedora é: %s\n", cidadeVencedora);
     
           //declaração de variaveis
-    char pais[50]= "rio de janeiro"; 
-    char pais2[50] = "São Paulo";
+    char pais[50]= "Pais 1"; 
+    char pais2[50] = "Pais 2";
     unsigned long int pop,pop2;
     int pturistico,pturistico2;
     float area,area2,pib,pib2;
-    float densidadep,densidadep2,pibpc,pibpc2,suppod,suppod2;
+    float densidadep,densidadep2,suppod,suppod2;
     pop = 6744000;
     pop2 = 11895578;
     pturistico = 54;
-    pturistico = 23;
-    area = 1200329;
+    pturistico2 = 23;
+    area = 1900329;
     area2 = 1521202;
     pib = 949301000000;
     pib2 = 1549301000000;  
     densidadep = pop / area;    //definição de variaveis 
     densidadep2 = pop2 / area2;
-    pibpc = pib / (float) pop;
-    pibpc2 = pib2 / (float) pop2;
+    int resultadoe,resultadoe2;
+    float somaA,somaB,somaA2,somaB2,resultadoFinalA,resultadoFinalB;
+
     
     printf("Carta1\n");
     printf("Nome da pais:%s\n", pais);
@@ -66,7 +67,7 @@ int main() {
     printf("PIB:%.2fbilhões de reais\n", pib);
     printf("Pontos Turísticos:%d\n", pturistico);
     printf("Densidade Populacional: %.2f hab/km²\n", densidadep);
-    printf("PIB per Capita: %.2f reais\n", pibpc);
+  
     
     printf("Carta2\n");
     printf("Nome da pais:%s\n", pais2);
@@ -75,12 +76,9 @@ int main() {
     printf("PIB:%.2fbilhões de reais\n", pib2);
     printf("Pontos Turísticos:%d\n", pturistico2);
     printf("Densidade Populacional: %.2f hab/km²\n", densidadep2);
-    printf("PIB per Capita: %.2f reais\n", pibpc2);
-    
-    suppod = (float) pop + area + pib + (float) pturistico + pibpc + 1/densidadep;  //definição do super poder
-    suppod2 = (float) pop2 + area2 + pib2 + (float) pturistico2 + pibpc2 + 1/densidadep2;
 
-    int escolhatabela;     //criação de variavel para armazenar escolha de opção de comparação
+    
+    int escolhatabela, escolhatabela2;     //criação de variavel para armazenar escolha de opção de comparação
     printf("\n=== MENU DE COMPARAÇÃO ===\n");
     printf("1. Comparar População\n");
     printf("2. Comparar Área\n");
@@ -88,83 +86,106 @@ int main() {
     printf("4. Comparar Pontos Turísticos\n");
     printf("5. Comparar Densidade Populacional\n");
     printf("6. Comparar Super Poder\n");
-    printf("Escolha o atributo para comparar (1-6): ");
+    printf("Escolha o primeiro atributo para comparação (1-6): ");
     scanf("%d", &escolhatabela);
-
+    
     switch (escolhatabela)      //switch para cada opção de comparação
     {
     case 1:             
-        printf("\nComparação de População:\n");
-        printf("%s: %lu hab\n", pais, pop);
-        printf("%s: %lu hab\n", pais2, pop2);
-        if(pop > pop2)
-            printf("%s Ganhou!\n", pais);
-        else if(pop2 > pop)
-            printf("%s Ganhou!\n", pais2);
-        else
-            printf("Empate!\n");
-        
+        printf("\nVocê escolheu comparação de População:\n");
+        resultadoe = pop > pop2 ? 1 : 0;
+        somaA = pop;
+        somaB = pop2;
         break;
     case 2:
-        printf("\nComparação de Área:\n");
-        printf("%s: %.2f km²\n", pais, area);
-        printf("%s: %.2f km²\n", pais2, area2);
-        if(area > area2)
-            printf("%s Ganhou!\n", pais);
-        else if(area2 > area)
-            printf("%s Ganhou!\n", pais2);
-        else
-            printf("Empate!\n");
+        printf("\nVocê escolheu Comparação de Área:\n");
+        resultadoe = area > area2 ? 1 : 0;
+        somaA = area;
+        somaB = area2;
         break;
     case 3:
-        printf("\nComparação de PIB:\n");
-        printf("%s: %.2f bilhões\n", pais, pib);
-        printf("%s: %.2f bilhões\n", pais2, pib2);
-        if(pib > pib2)
-            printf("%s Ganhou!\n", pais);
-        else if(pib2 > pib)
-            printf("%s Ganhou!\n", pais2);
-        else
-            printf("Empate!\n");
+        printf("\nVocê escolheu Comparação de PIB:\n");
+        resultadoe = pib > pib2 ? 1 : 0;
+        somaA = pib;
+        somaB = pib2;
         break;
     case 4:
-        printf("\nComparação de Pontos Turísticos:\n");
-        printf("%s: %d\n", pais, pturistico);
-        printf("%s: %d\n", pais2, pturistico2);
-        if(pturistico > pturistico2)
-            printf("%s Ganhou!\n", pais);
-        else if(pturistico2 > pturistico)
-            printf("%s Ganhou!\n", pais2);
-        else
-            printf("Empate!\n");
+        printf("\nVocê escolheu Comparação de Pontos Turísticos:\n");
+        resultadoe = pturistico > pturistico2 ? 1 : 0;
+        somaA = pturistico;
+        somaB = pturistico2;
         break;
     case 5:
-        printf("\nComparação de Densidade Populacional:\n");
-        printf("%s: %.2f hab/km²\n", pais, densidadep);
-        printf("%s: %.2f hab/km²\n", pais2, densidadep2);
-        if(densidadep < densidadep2)
-            printf("%s Ganhou!\n", pais);
-        else if(densidadep2 < densidadep)
-            printf("%s Ganhou!\n", pais2);
-        else
-            printf("Empate!\n");
+        printf("\nVocê escolheu Comparação de Densidade Populacional:\n");
+        resultadoe = densidadep < densidadep2 ? 1 : 0;
+        somaA = densidadep;
+        somaB = densidadep2;
         break;
-
-    case 6:
-        printf("\nComparação de Super Poder:\n");
-        printf("%s: %.2f\n", pais, suppod);
-        printf("%s: %.2f\n", pais2, suppod2);
-        if(suppod > suppod2)
-            printf("%s tem o maior Super Poder.\n", pais);
-        else if(suppod2 > suppod)
-            printf("%s tem o maior Super Poder.\n", pais2);
-        else
-            printf("Empate!\n");
-        break;
-    
     default:
+    printf("numero invalido, tente denovo.");
         break;
     }
-
+    
+    //segunda comparação de atributo
+    printf("\nEscolha o segundo atributo para comparação (1-6): ");
+    scanf("%d", &escolhatabela2);
+    if(escolhatabela == escolhatabela2){
+        printf("Você escolheu o mesmo atributo, tente denovo.");
+    }else{
+    switch (escolhatabela2)      //switch para cada opção de comparação
+    {
+    case 1:             
+        printf("\nVocê escolheu Comparação de População:\n");
+        resultadoe2 = pop > pop2 ? 1 : 0;
+        somaA2 = pop;
+        somaB2 = pop2;
+        break;
+    case 2:
+        printf("\nVocê escolheu Comparação de Área:\n");
+        resultadoe2 = area > area2 ? 1 : 0;
+        somaA2 = area;
+        somaB2 = area2;
+        break;
+    case 3:
+        printf("\nVocê escolheu Comparação de PIB:\n");
+        resultadoe2 = pib > pib2 ? 1 : 0;
+        somaA2 = pib;
+        somaB2 = pib2;
+        break;
+    case 4:
+        printf("\nVocê escolheu Comparação de Pontos Turísticos:\n");
+        resultadoe2 = pturistico > pturistico2 ? 1 : 0;
+        somaA2 = pturistico;
+        somaB2 = pturistico2;
+        break;
+    case 5:
+        printf("\nVocê escolheu Comparação de Densidade Populacional:\n");
+        resultadoe2 = densidadep < densidadep2 ? 1 : 0;
+        somaA2 = densidadep;
+        somaB2 = densidadep2;
+        break;
+    default:
+    printf("numero invalido, tente denovo.");
+        break;
+    }
+    if (resultadoe && resultadoe2){
+        printf("carta 1- %s: teve os maiores atributos.",pais);
+    }else if(resultadoe != resultadoe2){
+        printf("Empate entre atributos!");
+    }else{ 
+        printf("carta 2 - %s: teve os maiores atributos.", pais2);
+    }
+    resultadoFinalA = somaA + somaA2;
+    resultadoFinalB = somaB + somaB2;
+    printf("\nCarta 1 = %f\nCarta 2 = %f\n",resultadoFinalA,resultadoFinalB);
+    if (resultadoFinalA > resultadoFinalB){
+        printf("Carta 1- %s: Venceu o resultado final!",pais);
+    }else if(resultadoFinalA ==resultadoFinalB){
+        printf("Empate!");
+    }else{
+        printf("Carta 2 - %s: Venceu o resultado final!",pais2);
+    }
+    }
+    
     return 0;
 }
